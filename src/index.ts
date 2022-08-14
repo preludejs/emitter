5e087a5b5b05116e19986804b44bf379bfe499ee
Mirek Rusin <mirek@me.com>
2022-08-14 14:45:22 +0200

    feat(once): adding once

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import Emitter from './emitter.js'
 import listen from './listen.js'
 import of from './of.js'
 import on from './on.js'
+import once from './once.js'
 
 export {
   Emitter as t,
   Emitter,
   listen,
   of,
-  on
+  on,
+  once
 }
