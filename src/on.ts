acf8e3b2751ef8d36488fba130c37fba057c4253
Mirek Rusin <mirek@me.com>
2022-08-14 14:46:04 +0200

    style: type parameters in single line

--- src/on.ts
+++ src/on.ts
@@ -1,10 +1,7 @@
 import type { TypedEmitter, Listeners, UntypedListeners } from './emitter.js'
 
 const on =
-  <
-    U extends keyof T,
-    T extends Listeners<T> = UntypedListeners,
-  >(
+  <U extends keyof T, T extends Listeners<T> = UntypedListeners>(
     emitter: TypedEmitter<T>,
     event: U,
     listener: T[U]
