75b35ea755c4cece50f9c7744920d1d85bc652ff
Mirek Rusin <mirek@me.com>
2022-08-14 14:51:08 +0200

    feat(once-if): adding once-if

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import listen from './listen.js'
 import of from './of.js'
 import on from './on.js'
 import once from './once.js'
+import onceIf from './once-if.js'
 
 export {
   Emitter as t,
@@ -10,5 +11,6 @@ export {
   listen,
   of,
   on,
-  once
+  once,
+  onceIf
 }
